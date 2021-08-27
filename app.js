51da53f174733a02192028bb70ed833e3eb83e6f
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-08-27 11:03:49 -0700

    output div wired

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const btnTranslate = document.querySelector("#btn-translate");
 const txtInput = document.querySelector("#text-input");
+const outputTxt = document.querySelector("#output");
+
 
 function clickHandler() {
     console.log("Button was clicked");
-    console.log(txtInput.value);
+    // console.log(txtInput.value);
+    outputTxt.innerText= "Translated: asdasdadadasd  " + txtInput.value ;
+
 }
 
 btnTranslate.addEventListener("click", clickHandler); 
 
+// outputTxt.innerText = "Ruchi Goswami"; 
+
+/*
+"textarea"
+.btn-primary
+#input-btn
+*/
